212d9e91ac8b8d83a460a5efa622c2631924eb77
Khalil Fazal <khalil.fazal@uoit.net>
2016-12-16 10:28:37 -0500

    initialized day 2

---
+++ lib/adventofcode/years/2016/day_2.rb
@@ -0,0 +1,7 @@
+require 'monkey_patches/string'
+
+require_relative '2016'
+
+module Adventofcode::Year_2016::Day_2
+
+end
---
+++ test/years/2016/day_2_test.rb
@@ -0,0 +1,7 @@
+require 'adventofcode/years/2016/day_2'
+
+include Adventofcode::Year_2016::Day_2
+
+class Adventofcode::Year_2016::Day_2_Test < Test::Unit::TestCase
+
+end
