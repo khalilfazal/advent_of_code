836037c355ee10ad3c6e993e634011cffd18ca42
Khalil Fazal <khalil.fazal@uoit.net>
2016-12-17 16:00:03 -0500

    solved 2016 day 2

--- lib/adventofcode/years/2016/day_2.rb
+++ lib/adventofcode/years/2016/day_2.rb
@@ -4,7 +4,7 @@ module Adventofcode::Year_2016::Day_2
   class Keypad
     include Adventofcode::Year_2016
 
-    def self.bathroom_code(instructions = get_input, layout = imagined_layout)
+    def self.bathroom_code(instructions: get_input, layout: imagined_layout)
       keypad       = Keypad.new layout: layout
       instructions = instructions.lines if instructions.is_a? String
 
--- test/years/2016/day_2_test.rb
+++ test/years/2016/day_2_test.rb
@@ -13,25 +13,33 @@ class Adventofcode::Year_2016::Day_2_Test < Test::Unit::TestCase
       LURDL
       UUUUD
     )
-  end
 
-  def test_example_1
-    assert_equal_strings '1985', Keypad.bathroom_code(@example_instructions)
-  end
-
-  def test_1
-    omit_when_dced do
-      assert_equal '76792', Keypad.bathroom_code
-    end
-  end
-
-  def test_example_2
-    assert_equal_strings '5DB3', Keypad.bathroom_code(@example_instructions, [
+    @actual_layout = [
         '  1  ',
         ' 234 ',
         '56789',
         ' ABC ',
         '  D  '
-    ].join)
+    ].join
+  end
+
+  def test_example_1
+    assert_equal_strings '1985', Keypad.bathroom_code(instructions: @example_instructions)
+  end
+
+  def test_1
+    omit_when_dced do
+      assert_equal_strings '76792', Keypad.bathroom_code
+    end
+  end
+
+  def test_example_2
+    assert_equal_strings '5DB3', Keypad.bathroom_code(instructions: @example_instructions, layout: @actual_layout)
+  end
+
+  def test_2
+    omit_when_dced do
+      assert_equal_strings 'A7AC3', Keypad.bathroom_code(layout: @actual_layout)
+    end
   end
 end
