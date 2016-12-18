cfa75cfc829c2b08fac98cf4a0b55c978f2bade6
Khalil Fazal <khalil.fazal@uoit.net>
2016-12-18 00:31:37 -0500

    throw SystemCallError instead of unportable Errno::ENOENT

--- lib/adventofcode.rb
+++ lib/adventofcode.rb
@@ -1,6 +1,5 @@
 require 'adventofcode/version'
 require 'monkey_patches/array'
-require 'monkey_patches/errno'
 require 'open-uri'
 
 module Adventofcode
@@ -13,11 +12,11 @@ module Adventofcode
         open('cookie.txt', 'r') do |file|
           contents = file.read
 
-          raise Errno::ENOENT unless contents =~ /^session=[a-f0-9]+$/
+          raise SystemCallError unless contents =~ /^session=[a-f0-9]+$/
 
           contents
         end
-      rescue Errno::ENOENT
+      rescue SystemCallError
         raise StandardError.new [
             'Place your session cookie into cookie.txt',
             'See cookie.txt.sample'
