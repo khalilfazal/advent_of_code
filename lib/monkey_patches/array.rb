067c3f47ebd53b4c2e20a014a23412abe031134f
Khalil Fazal <khalil.fazal@uoit.net>
2016-12-20 10:12:34 -0500

    added Array::singleton and Array#join_with_prefix

--- lib/monkey_patches/array.rb
+++ lib/monkey_patches/array.rb
@@ -1,8 +1,18 @@
 class Array
   alias_method :old_join, :join
 
+  def self.singleton(e)
+    [] << e
+  end
+
   def join(separator = $,)
     '' + old_join(separator)
+  end
+
+  def join_with_prefix(prefix = '', separator = '')
+    map do |e|
+      prefix.to_s + e.to_s
+    end.join separator
   end
 
   def unlines
