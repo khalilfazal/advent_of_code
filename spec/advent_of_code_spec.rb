d4497026fbebe2cf8afbd270a85fb14ef18793dc
Khalil Fazal <khalil.fazal@uoit.net>
2016-12-22 01:16:27 -0500

    added spec for invalid year/day

--- spec/advent_of_code_spec.rb
+++ spec/advent_of_code_spec.rb
@@ -1,3 +1,5 @@
+require 'advent_of_code'
+
 describe 'AdventOfCode' do
   subject { AdventOfCode }
 
@@ -11,5 +13,9 @@ describe 'AdventOfCode' do
 
   it "'s ::cookie method is private" do
     expect { subject::COOKIE }.to raise_exception NameError
+  end
+
+  it 'throws a 404 Exception for an invalid year/day' do
+    expect { subject.input year: 0, day: 0 }.to raise_exception OpenURI::HTTPError
   end
 end
