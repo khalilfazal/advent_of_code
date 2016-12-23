3da872a68633f625896c8521e12b1d9dc21c707b
Khalil Fazal <khalil.fazal@uoit.net>
2016-12-23 11:32:51 -0500

    make method work in nested contexts

--- spec/helpers/cache_input.rb
+++ spec/helpers/cache_input.rb
@@ -3,13 +3,9 @@ module CacheInput
   MODULE_REGEX = /[_A-Za-z][_A-Za-z0-9]*/
 
   def cache_input
-    day = /(\d+)::#{MODULE_REGEX}$/.match(self.class.name)[1]
+    day = /Day(\d+)(?:::#{MODULE_REGEX})*$/.match(self.class.name)[1]
 
-    begin
-      Object.const_get("Day#{day}").input
-    rescue SocketError
-      nil
-    end
+    Object.const_get("Day#{day}").input
   end
 end
 
