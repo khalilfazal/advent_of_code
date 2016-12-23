a8867eeb434b760cbe948007a480a08f7a714294
Khalil Fazal <khalil.fazal@uoit.net>
2016-12-23 11:33:08 -0500

    added stubs for 2016 day 4

---
+++ lib/advent_of_code/year_2016/day_4/day_4.rb
@@ -0,0 +1,11 @@
+require 'advent_of_code/year_2016/year_2016'
+
+module Day4
+  include Year_2016
+
+  module_function
+
+  def input
+    Year_2016.input day: 4
+  end
+end
---
+++ spec/advent_of_code/year_2016/day_4_spec.rb
@@ -0,0 +1,5 @@
+require_all 'lib/advent_of_code/year_2016/day_4/*'
+
+describe 'Year2016_Day4' do
+  let (:input) { cache_input }
+end
