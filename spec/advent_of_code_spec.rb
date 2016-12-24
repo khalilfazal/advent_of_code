6582866772948377a3faa9e9e1acf8a91bddc0fa
Khalil Fazal <khalil.fazal@uoit.net>
2016-12-24 00:17:06 -0500

    using factory method for context '404 Exceptions for an invalid year/day'

--- spec/advent_of_code_spec.rb
+++ spec/advent_of_code_spec.rb
@@ -3,33 +3,37 @@ describe 'AdventOfCode' do
 
   it { within_block_is_expected.not_to raise_exception }
 
-  it "'s @@cookie is private" do
+  it "'s ::COOKIE const is private" do
     expect { subject::COOKIE }.to raise_exception NameError
   end
 
   it "'s ::cookie method is private" do
-    expect { subject::COOKIE }.to raise_exception NameError
+    expect { subject::cookie }.to raise_exception NameError
   end
 
   context '404 Exceptions for an invalid year/day' do
+    def expect_input_factory(year:, day:)
+      expect { subject.input year: year, day: day }.to raise_exception OpenURI::HTTPError
+    end
+
     it 'throws a 404 Exception when year is negative' do
-      expect { subject.input year: -1, day: 1 }.to raise_exception OpenURI::HTTPError
+      expect_input_factory year: -1, day: 1
     end
 
     it 'throws a 404 Exception when year < 2015' do
-      expect { subject.input year: 2014, day: 1 }.to raise_exception OpenURI::HTTPError
+      expect_input_factory year: 2014, day: 1
     end
 
     it 'throws a 404 Exception when day is negative' do
-      expect { subject.input year: 2015, day: -1 }.to raise_exception OpenURI::HTTPError
+      expect_input_factory year: 2015, day: -1
     end
 
     it 'throws a 404 Exception when day is 0' do
-      expect { subject.input year: 2015, day: 0 }.to raise_exception OpenURI::HTTPError
+      expect_input_factory year: 2015, day: 0
     end
 
     it 'throws a 404 Exception when day is > 25' do
-      expect { subject.input year: 2015, day: 26 }.to raise_exception OpenURI::HTTPError
+      expect_input_factory year: 2015, day: 26
     end
   end
 end
