ca5463deab0f717b8a0cd1458a605564ad67239f
Khalil Fazal <khalil.fazal@uoit.net>
2016-12-29 16:28:49 -0500

    disable slow tests

--- spec/advent_of_code/year_2016/day_5_spec.rb
+++ spec/advent_of_code/year_2016/day_5_spec.rb
@@ -13,22 +13,22 @@ describe Day5 do
   context 'examples and stars' do
     subject { MD5Hasher }
 
-    it 'example 1' do
+    xit 'example 1' do
       # noinspection RubyResolve
       expect(subject.crack example).to be_identical_to_string '18f47a30'
     end
 
-    it 'test 1' do
+    xit 'test 1' do
       # noinspection RubyResolve
       expect(subject.crack input).to be_identical_to_string '1a3099aa'
     end
 
-    it 'example 2' do
+    xit 'example 2' do
       # noinspection RubyResolve
       expect(subject.crack2 example).to be_identical_to_string '05ace8e3'
     end
 
-    it 'test 2' do
+    xit 'test 2' do
       # noinspection RubyResolve
       expect(subject.crack2 input).to be_identical_to_string '694190cd'
     end
