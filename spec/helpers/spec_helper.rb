04dedce2af682162f6252a2a0c97557b9a1bce0e
Khalil Fazal <khalil.fazal.0@gmail.com>
2017-01-02 02:58:13 -0500

    Update spec_helper.rb

--- spec/helpers/spec_helper.rb
+++ spec/helpers/spec_helper.rb
@@ -2,9 +2,14 @@ require 'advent_of_code'
 require 'helpers/match_string'
 require 'helpers/skip'
 require 'helpers/within_block_is_expected'
-require 'simplecov'
 
-SimpleCov.start
+if ENV['CI']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+else
+  require 'simplecov'
+  SimpleCov.start
+end
 
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
