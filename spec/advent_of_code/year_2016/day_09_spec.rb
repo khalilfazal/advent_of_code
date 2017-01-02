1fd2baec86570899d62f4cc5e3d4862ca758b823
Khalil Fazal <khalil.fazal@uoit.net>
2017-01-02 02:37:41 -0500

    solved issue found by codeclimate: 'Similar code found in 5 other locations'

--- spec/advent_of_code/year_2016/day_09_spec.rb
+++ spec/advent_of_code/year_2016/day_09_spec.rb
@@ -9,52 +9,40 @@ include AdventOfCode::Year2016
 describe Day9 do
   let(:input) { @input ||= described_class.input.delete "\n" }
 
-  it 'example 1' do
-    file = Day9::File.new 'ADVENT'
+  examples = [{
+    compressed: 'ADVENT',
+    decompressed: 'ADVENT',
+    length: 6
+  }, {
+    compressed: 'A(1x5)BC',
+    decompressed: 'ABBBBBC',
+    length: 7
+  }, {
+    compressed: '(3x3)XYZ',
+    decompressed: 'XYZXYZXYZ',
+    length: 9
+  }, {
+    compressed: 'A(2x2)BCD(2x2)EFG',
+    decompressed: 'ABCBCDEFEFG',
+    length: 11
+  }, {
+    compressed: '(6x1)(1x3)A',
+    decompressed: '(1x3)A',
+    length: 6
+  }, {
+    compressed: 'X(8x2)(3x3)ABCY',
+    decompressed: 'X(3x3)ABC(3x3)ABCY',
+    length: 18
+  }]
 
-    # noinspection RubyResolve
-    expect(file.decompress).to be_identical_to_string 'ADVENT'
-    expect(file.length).to be === 6
-  end
+  examples.each_with_index do |example, i|
+    it "example #{i + 1}" do
+      file = Day9::File.new example[:compressed]
 
-  it 'example 2' do
-    file = Day9::File.new 'A(1x5)BC'
-
-    # noinspection RubyResolve
-    expect(file.decompress).to be_identical_to_string 'ABBBBBC'
-    expect(file.length).to be === 7
-  end
-
-  it 'example 3' do
-    file = Day9::File.new '(3x3)XYZ'
-
-    # noinspection RubyResolve
-    expect(file.decompress).to be_identical_to_string 'XYZXYZXYZ'
-    expect(file.length).to be === 9
-  end
-
-  it 'example 4' do
-    file = Day9::File.new 'A(2x2)BCD(2x2)EFG'
-
-    # noinspection RubyResolve
-    expect(file.decompress).to be_identical_to_string 'ABCBCDEFEFG'
-    expect(file.length).to be === 11
-  end
-
-  it 'example 5' do
-    file = Day9::File.new '(6x1)(1x3)A'
-
-    # noinspection RubyResolve
-    expect(file.decompress).to be_identical_to_string '(1x3)A'
-    expect(file.length).to be === 6
-  end
-
-  it 'example 6' do
-    file = Day9::File.new 'X(8x2)(3x3)ABCY'
-
-    # noinspection RubyResolve
-    expect(file.decompress).to be_identical_to_string 'X(3x3)ABC(3x3)ABCY'
-    expect(file.length).to be === 18
+      # noinspection RubyResolve
+      expect(file.decompress).to be_identical_to_string example[:decompressed]
+      expect(file.length).to be === example[:length]
+    end
   end
 
   it 'test 1' do
