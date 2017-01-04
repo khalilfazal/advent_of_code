05897201b08c5f2888d93c403c395bea17186592
Khalil Fazal <khalil.fazal@uoit.net>
2017-01-04 05:05:19 -0500

    moved seeds folder to db/

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 autoload :AdventProblem, 'models/advent_problem'
 autoload :JSON, 'json'
 
-JSON.parse(File.read 'lib/seeds/solutions.json').each do |h|
+JSON.parse(File.read 'db/seeds/solutions.json').each do |h|
   AdventProblem.find_or_create_by!(h.slice 'year', 'day').tap do |problem|
     problem.update solutions: h['solutions'].map(&:inspect)
   end
