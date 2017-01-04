70d804f13a5559a8ee7ec3297f65cd2acf55f981
Khalil Fazal <khalil.fazal@uoit.net>
2017-01-04 04:23:03 -0500

    added seed for solutions column

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+autoload :AdventProblem, 'models/advent_problem'
+autoload :JSON, 'json'
+
+JSON.parse(File.read 'lib/seeds/solutions.json').each do |h|
+  AdventProblem.find_or_create_by!(h.slice 'year', 'day').tap do |problem|
+    problem.update solutions: h['solutions'].map(&:inspect)
+  end
+end
