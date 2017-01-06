7dc06248b1dcff6f451dbe819aee38c6cfa174e8
Khalil Fazal <khalil.fazal@uoit.net>
2017-01-05 20:44:14 -0500

    resolved rubocop messages

--- lib/monkey_patches/float.rb
+++ lib/monkey_patches/float.rb
@@ -1,11 +1,15 @@
 autoload :ChronicDuration, 'chronic_duration'
 
+# added methods to format duration
 class Float
+  # Shows the duration in human readable format
+  #
+  # @return String
   def duration
     ChronicDuration.output self
   end
 
   def format(format)
-    (sprintf format, self).to_f
+    (format % self).to_f
   end
 end
--- lib/monkey_patches/travis.rb
+++ lib/monkey_patches/travis.rb
@@ -1,5 +1,11 @@
-class Travis::Client::Build
-  def to_s
-    "build ##{number}"
+module Travis
+  module Client
+    # a build attempt
+    class Build
+      # @return String
+      def to_s
+        "build ##{number}"
+      end
+    end
   end
 end
