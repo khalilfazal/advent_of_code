f70017c3806e92dc998d8fd478dc4493778a3897
Khalil Fazal <khalil.fazal@uoit.net>
2017-01-06 21:35:56 -0500

    changed class Day9::File to module Decompressor

---
+++ lib/advent_of_code/year_2016/day_09/decompressor.rb
@@ -0,0 +1,57 @@
+require 'monkey_patches/string'
+
+# Decompresses compressed
+module Decompressor
+  module_function
+
+  # Partially decompresses the +compressed+
+  # by checking for only one layer of compression
+  #
+  # @param compressed String
+  #
+  # @return String
+  def partially_decompress(compressed)
+    if (parsed = compressed.match /(.*?)\((\d+)x(\d+)\)(.*)/)
+      safe, length, times, rest = parsed.captures
+      length = length.to_i
+      times = times.to_i
+
+      left, right = rest.split_at length
+      safe + left * times + partially_decompress(right)
+    else
+      compressed
+    end
+  end
+
+  # Calculates the total length of the compressed when fully decompressed.
+  #
+  # @param compressed String
+  #
+  # @return Integer
+  def full_decompressed_length(compressed)
+    total_length = 0
+    min_length = compressed.length
+    weights = [1] * min_length
+    i = 0
+
+    while i < min_length
+      if compressed[i] === '('
+        length, times = compressed.slice(i .. -1).match(/\((\d+)x(\d+)\)/).captures
+        marker_end = i + 3 + length.length + times.length
+        length = length.to_i
+        times = times.to_i
+
+        (marker_end ... marker_end + length).each do |j|
+          weights[j] *= times
+        end
+
+        i = marker_end
+      else
+        total_length += weights[i]
+        i += 1
+      end
+    end
+
+    total_length
+  end
+end
--- spec/advent_of_code/year_2016/day_09_spec.rb
+++ spec/advent_of_code/year_2016/day_09_spec.rb
@@ -29,23 +29,23 @@ describe AdventOfCode::Year2016::Day9 do
       decompressed: 'X(3x3)ABC(3x3)ABCY'
     }].each_with_index do |example, i|
       it "example #{i + 1}" do
-        file = Day9::File.new example[:compressed]
+        decompressed = Decompressor.partially_decompress example[:compressed]
 
         # noinspection RubyResolve
-        expect(file.decompress).to be_identical_to_string example[:decompressed]
-        expect(file.length).to be === example[:decompressed].length
+        expect(decompressed).to be_identical_to_string example[:decompressed]
+        expect(decompressed.length).to be === example[:decompressed].length
       end
     end
 
     it 'achieves star 1' do
       skip_this_when_dced do
-        expect(Day9::File.new(input).decompress.length).to be === problem.solution(1)
+        expect(Decompressor.partially_decompress(input).length).to be === problem.solution(1)
       end
     end
 
     it 'achieves star 2' do
       skip_this_when_dced do
-        expect(Day9::File.decompressed_length input).to be === problem.solution(2)
+        expect(Decompressor.full_decompressed_length input).to be === problem.solution(2)
       end
     end
   end
