c7c471cdbb73ae8e6351558dbdc93d428ddec08b
Khalil Fazal <khalil.fazal@uoit.net>
2017-01-07 13:25:36 -0500

    implemented String#eql? to check encoding

--- lib/monkey_patches/string.rb
+++ lib/monkey_patches/string.rb
@@ -3,6 +3,8 @@ require 'English'
 
 # Reimplemented String#lines which doesn't remove newline characters
 class String
+  alias old_eql? eql?
+
   # @param separator String
   #
   # @return [String]
@@ -10,10 +12,24 @@ class String
     split separator
   end
 
+  # Returns an array of size two where the first element is a prefix of length n
+  # and the second is the rest of the string
+  #
   # @param n Integer
   #
   # @return [String]
   def split_at(n)
     [slice!(0 .. n - 1), self]
   end
+
+  # If two strings are equal, they must have the same encoding
+  #
+  # @param other String
+  #
+  # @return Boolean
+  def eql?(other)
+    old_eql?(other) && encoding.eql?(other.encoding)
+  end
+
+  private
 end
