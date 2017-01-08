bfec314ef1b6915b03b3960a294086bcbfcf9a36
Khalil Fazal <khalil.fazal@uoit.net>
2017-01-07 21:49:12 -0500

    Improved documentation for Output

--- lib/advent_of_code/year_2016/day_10/output.rb
+++ lib/advent_of_code/year_2016/day_10/output.rb
@@ -1,5 +1,8 @@
 # Can't give to other Entities or make promises
 class Output < Entity
+  # Gets the microchip in the output
+  # Assuming that the +Output+ only receives one microchip
+  #
   # @return Integer
   def output
     # noinspection RubyResolve
