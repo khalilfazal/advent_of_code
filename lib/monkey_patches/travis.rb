92af7f2116ccc2355301be2615a1c83ba0b5a9a1
Khalil Fazal <khalil.fazal@uoit.net>
2017-01-07 20:16:43 -0500

    Improved documentation for Travis::Client::Build#to_s

--- lib/monkey_patches/travis.rb
+++ lib/monkey_patches/travis.rb
@@ -2,6 +2,8 @@ module Travis
   module Client
     # a build attempt
     class Build
+      # Represents a build as a string showing its build number
+      #
       # @return String
       def to_s
         "build ##{number}"
