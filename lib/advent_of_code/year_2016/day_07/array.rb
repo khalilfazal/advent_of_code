17e0c9d5995bcb4b4223e34d42dab002e205700e
Khalil Fazal <khalil.fazal@uoit.net>
2017-01-08 01:30:49 -0500

    Improved documentation for Array

--- lib/advent_of_code/year_2016/day_07/array.rb
+++ lib/advent_of_code/year_2016/day_07/array.rb
@@ -3,7 +3,9 @@ require 'monkey_patches/string'
 # I think using methods look neater than using functions
 # Object.m() versus f(Object)
 class Array
-  # @return [Element]
+  # Finds all aba sequences in the +Array+
+  #
+  # @return Array of Element
   def abas
     map do |sequence|
       sequence.substrings_of(3).find_all do |a, b, c|
